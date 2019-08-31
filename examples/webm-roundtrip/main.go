ea74a085d9b44304f497fd4e6dbc1245eb0f2b86
Atsushi Watanabe <atsushi.w@ieee.org>
2019-08-31 15:49:42 +0900

    Update example to roundtrip webm

--- examples/webm-roundtrip/main.go
+++ examples/webm-roundtrip/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/at-wat/ebml-go"
 )
@@ -13,9 +14,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer r.Close()
 
 	var ret struct {
 		Header struct {
+			EBMLVersion        uint64 `ebml:"EBMLVersion"`
+			EBMLReadVersion    uint64 `ebml:"EBMLReadVersion"`
+			EBMLMaxIDLength    uint64 `ebml:"EBMLMaxIDLength"`
+			EBMLMaxSizeLength  uint64 `ebml:"EBMLMaxSizeLength"`
 			DocType            string `ebml:"EBMLDocType"`
 			DocTypeVersion     uint64 `ebml:"EBMLDocTypeVersion"`
 			DocTypeReadVersion uint64 `ebml:"EBMLDocTypeReadVersion"`
@@ -28,17 +34,24 @@ func main() {
 				} `ebml:"Seek"`
 			} `ebml:"SeekHead"`
 			Info struct {
-				TimecodeScale uint64 `ebml:"TimecodeScale"`
-				MuxingApp     string `ebml:"MuxingApp"`
-				WritingApp    string `ebml:"WritingApp"`
+				TimecodeScale uint64    `ebml:"TimecodeScale"`
+				MuxingApp     string    `ebml:"MuxingApp"`
+				WritingApp    string    `ebml:"WritingApp"`
+				Duration      float64   `ebml:"Duration"`
+				DateUTC       time.Time `ebml:"DateUTC"`
 			} `ebml:"Info"`
 			Tracks struct {
 				TrackEntry []struct {
-					TrackNumber uint64 `ebml:"TrackNumber"`
-					TrackUID    uint64 `ebml:"TrackUID"`
-					CodecID     string `ebml:"CodecID"`
-					TrackType   uint64 `ebml:"TrackType"`
-					Audio       struct {
+					Name            string `ebml:"Name"`
+					TrackNumber     uint64 `ebml:"TrackNumber"`
+					TrackUID        uint64 `ebml:"TrackUID"`
+					CodecID         string `ebml:"CodecID"`
+					CodecPrivate    []byte `ebml:"CodecPrivate"`
+					CodecDelay      uint64 `ebml:"CodecDelay"`
+					TrackType       uint64 `ebml:"TrackType"`
+					DefaultDuration uint64 `ebml:"DefaultDuration"`
+					SeekPreRoll     uint64 `ebml:"SeekPreRoll"`
+					Audio           struct {
 						SamplingFrequency float64 `ebml:"SamplingFrequency"`
 						Channels          uint64  `ebml:"Channels"`
 					} `ebml:"Audio"`
@@ -67,5 +80,14 @@ func main() {
 		fmt.Printf("error: %v\n", err)
 		return
 	}
-	fmt.Print(string(j))
+	fmt.Printf("%s\n", string(j))
+
+	w, err := os.OpenFile("copy.webm", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer w.Close()
+	if err := ebml.Marshal(&ret, w); err != nil {
+		panic(err)
+	}
 }
