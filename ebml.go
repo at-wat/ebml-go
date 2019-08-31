75bbd7d1442138f117dff740a8e4843324db3262
Atsushi Watanabe <atsushi.w@ieee.org>
2019-09-01 02:28:06 +0900

    Update README and license header (#26)

--- ebml.go
+++ ebml.go
@@ -12,42 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package ebml implements encoding and decoding of Extensible Binary Meta Language (EBML).
 package ebml
-
-// Type represents EBML Element data type
-type Type int
-
-// EBML Element data types
-const (
-	TypeMaster = iota
-	TypeInt
-	TypeUInt
-	TypeDate
-	TypeFloat
-	TypeBinary
-	TypeString
-	TypeBlock
-)
-
-func (t Type) String() string {
-	switch t {
-	case TypeMaster:
-		return "Master"
-	case TypeInt:
-		return "Int"
-	case TypeUInt:
-		return "UInt"
-	case TypeDate:
-		return "Date"
-	case TypeFloat:
-		return "Float"
-	case TypeBinary:
-		return "Binary"
-	case TypeString:
-		return "String"
-	case TypeBlock:
-		return "Block"
-	default:
-		return "Unknown type"
-	}
-}
