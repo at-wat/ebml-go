6025def6f9572e56ba6396ceddeb088525c7fd92
Atsushi Watanabe <atsushi.w@ieee.org>
2019-09-03 01:01:52 +0900

    Add tests

---
+++ webm/sizedwriter_test.go
@@ -0,0 +1,46 @@
+package webm
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+type bufferCloser struct {
+	bytes.Buffer
+	closed chan struct{}
+}
+
+func (b *bufferCloser) Close() error {
+	close(b.closed)
+	return nil
+}
+
+func TestWriterWithSizeCount(t *testing.T) {
+	buf := &bufferCloser{closed: make(chan struct{})}
+	w := &writerWithSizeCount{w: buf}
+
+	if n, err := w.Write([]byte{0x01, 0x02}); err != nil {
+		t.Fatalf("Failed to Write: %v", err)
+	} else if n != 2 {
+		t.Errorf("Unexpected return value of writerWithSizeCount.Write, expected: 2, got: %d", n)
+	}
+	if n := w.Size(); n != 2 {
+		t.Errorf("Unexpected return value of writerWithSizeCount.Size(), expected: 2, got: %d", n)
+	}
+
+	w.Clear()
+
+	if n := w.Size(); n != 0 {
+		t.Errorf("Unexpected return value of writerWithSizeCount.Size(), expected: 0, got: %d", n)
+	}
+
+	if err := w.Close(); err != nil {
+		t.Errorf("writerWithSizeCount.Close() doesn't propagate base io.WriteCloser.Close() return value")
+	}
+	select {
+	case <-buf.closed:
+	case <-time.After(time.Second):
+		t.Errorf("Base io.WriteCloser is not closed by writerWithSizeCount.Close()")
+	}
+}
---
+++ webm/writer_test.go
@@ -0,0 +1,127 @@
+package webm
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/at-wat/ebml-go"
+)
+
+func TestSimpleWriter(t *testing.T) {
+	buf := &bufferCloser{closed: make(chan struct{})}
+
+	tracks := []TrackEntry{
+		{
+			Name:        "Video",
+			TrackNumber: 1,
+			TrackUID:    12345,
+			CodecID:     "V_VP8",
+			TrackType:   1,
+			Video: &Video{
+				PixelWidth:  320,
+				PixelHeight: 240,
+			},
+		},
+		{
+			Name:        "Audio",
+			TrackNumber: 2,
+			TrackUID:    54321,
+			CodecID:     "V_OPUS",
+			TrackType:   2,
+			Audio: &Audio{
+				SamplingFrequency: 48000.0,
+				Channels:          2,
+			},
+		},
+	}
+	ws, err := NewSimpleWriter(buf, tracks)
+	if err != nil {
+		t.Fatalf("Failed to create SimpleWriter: %v", err)
+	}
+
+	if len(ws) != len(tracks) {
+		t.Fatalf("Number of the returned writer (%d) must be same as the number of TrackEntry (%d)", len(ws), len(tracks))
+	}
+
+	if n, err := ws[0].Write(false, 100, []byte{0x01, 0x02}); err != nil {
+		t.Fatalf("Failed to Write: %v", err)
+	} else if n != 2 {
+		t.Errorf("Unexpected return value of FrameWriter.Write, expected: 2, got: %d", n)
+	}
+
+	if n, err := ws[1].Write(true, 110, []byte{0x03, 0x04, 0x05}); err != nil {
+		t.Fatalf("Failed to Write: %v", err)
+	} else if n != 3 {
+		t.Errorf("Unexpected return value of FrameWriter.Write, expected: 3, got: %d", n)
+	}
+
+	if n, err := ws[0].Write(true, 130, []byte{0x06}); err != nil {
+		t.Fatalf("Failed to Write: %v", err)
+	} else if n != 1 {
+		t.Errorf("Unexpected return value of FrameWriter.Write, expected: 1, got: %d", n)
+	}
+
+	ws[0].Close()
+	ws[1].Close()
+	select {
+	case <-buf.closed:
+	case <-time.After(time.Second):
+		t.Errorf("Base io.WriteCloser is not closed by SimpleWriter")
+	}
+
+	expected := struct {
+		Header  EBMLHeader `ebml:"EBML"`
+		Segment Segment    `ebml:"Segment,inf"`
+	}{
+		Header: defaultEBMLHeader,
+		Segment: Segment{
+			Info: defaultSegmentInfo,
+			Tracks: Tracks{
+				TrackEntry: tracks,
+			},
+			Cluster: []Cluster{
+				{
+					Timecode: uint64(0),
+					SimpleBlock: []ebml.Block{
+						{
+							TrackNumber: 1,
+							Timecode:    int16(0),
+							Keyframe:    false,
+							Data:        [][]byte{[]byte{0x01, 0x02}},
+						},
+						{
+							TrackNumber: 2,
+							Timecode:    int16(10),
+							Keyframe:    true,
+							Data:        [][]byte{[]byte{0x03, 0x04, 0x05}},
+						},
+						{
+							TrackNumber: 1,
+							Timecode:    int16(30),
+							Keyframe:    true,
+							Data:        [][]byte{[]byte{0x06}},
+						},
+					},
+				},
+				{
+					Timecode: uint64(30),
+					PrevSize: uint64(39),
+				},
+			},
+		},
+	}
+	defer func() {
+		var result struct {
+			Header  EBMLHeader `ebml:"EBML"`
+			Segment Segment    `ebml:"Segment,inf"`
+		}
+		if err := ebml.Unmarshal(bytes.NewReader(buf.Bytes()), &result); err != nil {
+			t.Fatalf("Failed to Unmarshal resultant binary: %v", err)
+		}
+		if !reflect.DeepEqual(expected, result) {
+			t.Errorf("Unexpected WebM data,\nexpected: %+v\n     got: %+v", expected, result)
+		}
+	}()
+}
