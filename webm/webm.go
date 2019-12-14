c3c62a7eea23d54a3cf301bf148b2f3504186496
Atsushi Watanabe <atsushi.w@ieee.org>
2019-12-14 22:39:34 +0900

    Update package description

--- webm/webm.go
+++ webm/webm.go
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package webm provides WebM multimedia writer.
+//
+// The package implements block data writer for multi-track WebM container.
 package webm
 
 import (
