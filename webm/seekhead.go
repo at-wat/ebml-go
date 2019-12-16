1260d8f76ce13aa3f4b6041b223c049e73c69780
Atsushi Watanabe <atsushi.w@ieee.org>
2019-12-16 11:39:47 +0900

    Fix SeekHead calculation

--- webm/seekhead.go
+++ webm/seekhead.go
@@ -64,8 +64,7 @@ func setSeekHead(header *flexHeader, opts ...ebml.MarshalOption) error {
 	var segmentPos uint64
 	hook := func(e *ebml.Element) {
 		switch e.Name {
-		case "SeekHead":
-			// SeekHead position is the top of the Segment contents.
+		case "Segment":
 			// Origin of the segment position is here.
 			segmentPos = e.Position
 		case "Info":
