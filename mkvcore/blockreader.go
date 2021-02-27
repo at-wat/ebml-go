b4241c27796d9e4890e4b62ee990eb5f5775c42a
Atsushi Watanabe <atsushi.w@ieee.org>
2021-02-27 09:55:02 +0900

    Fix copyright year

--- mkvcore/blockreader.go
+++ mkvcore/blockreader.go
@@ -1,4 +1,4 @@
-// Copyright 2019 The ebml-go authors.
+// Copyright 2020-2021 The ebml-go authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
