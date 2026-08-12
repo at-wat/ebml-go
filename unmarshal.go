c60324c5c6c86e3d39d03e1e4659008b5fb8b425
Atsushi Watanabe <atsushi.w@ieee.org>
2026-08-12 21:45:57 +0900

    Update doc

--- unmarshal.go
+++ unmarshal.go
@@ -30,7 +30,7 @@ var ErrIndefiniteType = errors.New("marshal/unmarshal to indefinite type")
 // ErrIncompatibleType means that an element is not convertible to a corresponding struct field.
 var ErrIncompatibleType = errors.New("marshal/unmarshal to incompatible type")
 
-// ErrInvalidElementSize means that an element has inconsistent size. e.g. element size is larger than its parent element size, or unknown size on non-master element.
+// ErrInvalidElementSize means that an element has an inconsistent size. e.g., element size is larger than its parent element size, invalid scalar size, or unknown size of a non-master element.
 var ErrInvalidElementSize = errors.New("invalid element size")
 
 // ErrReadStopped is returned if unmarshaler finished to read element which has stop tag.
