4407f401e65dce74cad5d325e9208045d6cdc2f5
Atsushi Watanabe <atsushi.w@ieee.org>
2026-08-26 14:08:28 +0900

    Update package comment

--- internal/buffercloser/buffercloser.go
+++ internal/buffercloser/buffercloser.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package buffercloser implements closable variant of bytes.Buffer for testing.
+// Package buffercloser implements a closable variant of bytes.Buffer for testing.
 package buffercloser
 
 import (
